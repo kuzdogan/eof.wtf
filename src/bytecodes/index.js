b56627344d92ad8554d3ab896a17ae90fbc15085
kuzdogan <kuzdogan@users.noreply.github.com>
2024-11-21 14:50:51 +0000

    deploy: ffaedaaa845b9bc11541358485842ae30875d8c2

--- src/bytecodes/index.js
+++
@@ -1,7 +0,0 @@
-import example1 from "./example1";
-import subcontainers from "./subcontainers";
-import simpleCreate from "./simpleCreate";
-import dataSection from "./dataSection";
-
-const bytecodes = { example1, subcontainers, simpleCreate, dataSection };
-export default bytecodes;
